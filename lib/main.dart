c1f7f1e3e607da43e74739187a27013cc74732db
Paul Halliday <hello@paulhalliday.io>
2020-11-23 11:01:19 +0000

    Updated dir

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:ds_flow_builder/src/contact_flow/pages/home_page.dart';
+import 'package:ds_flow_builder/src/home/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
